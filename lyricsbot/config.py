db1b33c35865af7a440b3bee587083c5377f35f7
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2019-08-27 12:43:51 -0300

    Update config.py

--- lyricsbot/config.py
+++ lyricsbot/config.py
@@ -5,9 +5,9 @@ import os
 
 
 if os.environ['ENVIRONMENT'] == 'production':
-    TOKEN = '555478289:AAHkplsoeKEf0xeJvVXrTeQ9oHMQ20hTxHI'
+    TOKEN = '736244244:AAGGaEdFAOtrP9YtLoJeJ3tQ8LtBuFGSbu4'
 
 if os.environ['ENVIRONMENT'] == 'local':
-    TOKEN = '574721215:AAGq4TJpcz-6qObpOF5gWymn-jvVBrWF2dY'
+    TOKEN = ':736244244:AAGGaEdFAOtrP9YtLoJeJ3tQ8LtBuFGSbu4'
 
 URL = 'postgres://rikdrycw:O_P2Qr5FcLWHAU6aU8h3GfiKwjhDJoYE@baasu.db.elephantsql.com:5432/rikdrycw'
